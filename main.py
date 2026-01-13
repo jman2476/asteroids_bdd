c6475609983cf809806a7c7d6be8ab5e045b2841
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-01-13 11:59:04 -0500

    add player class, render player in game loop

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Instantiation, before game loop
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     clock = pygame.time.Clock()
     delta_time = 0
-
+    print('Screen var', screen)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 print('Exiting game')
                 return
-
+        # Start display draw
         screen.fill('black')
-
         delta_time = clock.tick(60)/1000
-
+        player.draw(screen)
         # Refresh display
         pygame.display.flip()
 
