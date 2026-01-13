bd3c7a3b01457941701115d8a2659dfec02f9fd9
Squirblechem <152367545+jman2476@users.noreply.github.com>
2026-01-13 01:47:52 -0500

    add clock, delta_time

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    delta_time = 0
 
     while True:
         log_state()
@@ -18,6 +21,9 @@ def main():
                 return
 
         screen.fill('black')
+
+        delta_time = clock.tick(60)/1000
+
         # Refresh display
         pygame.display.flip()
 
